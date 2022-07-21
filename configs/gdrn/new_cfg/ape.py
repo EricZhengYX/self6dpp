0c24727cbec890bc09dfaf407927c36970205cb3
EricZhengYX <yuxiang.zheng@outlook.com>
2022-07-21 16:50:29 +0200

    0.12

--- configs/gdrn/new_cfg/ape.py
+++ configs/gdrn/new_cfg/ape.py
@@ -3,6 +3,6 @@ _base_ = ["LM_base.py"]
 OUTPUT_DIR = "output/gdrn/lm_pbr/new_cfg/ape"
 
 DATASETS = dict(
-    TRAIN=("lm_pbr_ape_train",),  # lm_pbr_ape_train, lm_real_ape_train
+    TRAIN=("lm_pbr_ape_train",),
     TEST=("lmo_ape_bop_test", "lm_real_ape_test"),
 )
